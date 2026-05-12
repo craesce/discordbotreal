3fcd8cdfc0db97be4eebf401ce250732f261f022
Christian Klein <kleinch@bbwnd.net>
2026-05-12 14:40:51 +0200

    6767

--- main.py
+++ main.py
@@ -1,44 +1,53 @@
-import platform
-import subprocess
+import random
+import logging
+import os
 
 import discord
 from discord.ext import commands
-import logging
 from dotenv import load_dotenv
-import os
-from getpass import getpass
 
 load_dotenv()
 token = os.getenv("DISCORD_TOKEN")
 
-handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
+handler = logging.FileHandler(
+    filename='discord.log',
+    encoding='utf-8',
+    mode='w'
+)
+
+
 intents = discord.Intents.default()
 intents.message_content = True
 intents.members = True
+intents.reactions = True
 
 bot = commands.Bot(command_prefix='/', intents=intents)
 
+combo_triggered_messages = set()
+
+
+image_map = {
+    "🐱": "img/rosi.jpg",
+    "🔥": "https://media.giphy.com/media/3o72F8t9TDi2xVnxOE/giphy.gif"
+}
+
+
 @bot.event
 async def on_message(message):
+
     if message.author == bot.user:
         return
+
     if bot.user.mentioned_in(message):
         await message.channel.send("Typeshit")
+
     if "job" in message.content.lower():
-           await message.channel.send(
-             "https://tenor.com/view/breaking-phone-glep-smiling-friends-destroying-phone-angry-gif-14103972177458051138")
+        await message.channel.send(
+            "https://tenor.com/view/breaking-phone-glep-smiling-friends-destroying-phone-angry-gif-14103972177458051138"
+        )
+
     await bot.process_commands(message)
-#python basics
 
-# string_number = '69'
-# item = 'banana'
-# Item = 'Apple'
-# list = [1,2,3,4,5,6,7,'piemel','banana'+item+Item]
-
-
-#hangman typeshit
-
-import random
 
 hangman_art = {
     0: (" |\n"
@@ -94,9 +103,19 @@ hangman_art = {
         " |   / \\\n")
 }
 
-words = ["banana", "apple", "discord", "python", "penis", "67", "glep", "typeshit"]
+words = [
+    "banana",
+    "apple",
+    "discord",
+    "python",
+    "penis",
+    "67",
+    "glep",
+    "typeshit"
+]
 
 games = {}
+
 
 @bot.command()
 async def hangman(ctx):
@@ -107,15 +126,18 @@ async def hangman(ctx):
         "answer": answer,
         "wrong_guesses": 0,
         "letters_guessed": set(),
-        "guesses": 0,
         "hidden_word": ["[ ]"] * len(answer)
     }
 
-    await ctx.send(
+    msg = await ctx.send(
         f"Hangman gestartet!\n"
         f"```{hangman_art[0]}```\n"
-        f"{' '.join(games[ctx.author.id]['hidden_word'])}"
+        f"{' '.join(games[ctx.author.id]['hidden_word'])}",
+        file=discord.File("img/default.jpg")
     )
+    for emoji in image_map.keys():
+        await msg.add_reaction(emoji)
+
 
 @bot.command()
 async def hguess(ctx, guessed_input):
@@ -131,7 +153,6 @@ async def hguess(ctx, guessed_input):
     hidden_word = game["hidden_word"]
 
     guessed_input = guessed_input.lower()
-
 
     if len(guessed_input) > 1:
 
@@ -177,7 +198,7 @@ async def hguess(ctx, guessed_input):
         del games[ctx.author.id]
         return
 
-    if wrong_guesses == len(hangman_art)-1:
+    if wrong_guesses == len(hangman_art) - 1:
 
         await ctx.send(
             f"```{hangman_art[wrong_guesses]}```\n"
@@ -194,45 +215,122 @@ async def hguess(ctx, guessed_input):
         f"Letters guessed: {', '.join(sorted(letters_guessed))}"
     )
 
+
 @bot.command()
 async def idee(ctx):
-    await ctx.send(
+    await ctx.send("die Idee nieder")
 
-        f"die Idee nieder")
 
-reaction_sessions = {}
-@bot.command()
-async def reaction(ctx):
 
-    embed = discord.Embed(
-        title = "Reaction",
-        description = "React with :cat:"
-    )
-
-    msg = await ctx.send(embed=embed)
-
-    await msg.add_reaction(":cat:")
-
-    reaction_sessions[msg.id] = True
-
-@bot.event()
+@bot.event
 async def on_reaction_add(reaction, user):
 
     if user.bot:
         return
 
     message = reaction.message
-
-    if message.id not in reaction_sessions:
-        return
-
     emoji = str(reaction.emoji)
 
-    responses = {
-        ":cat:": "C:\Users\KleinCh\PycharmProjects\clean-repo\img\rosi.jpg"
+    image_map = {
+        "🐱": "img/rosi.jpg",
+        "🔥": "https://media2.giphy.com/media/v1.Y2lkPTc5MGI3NjExZzY1YXEzbjNpcDFiNjNzajQ3MXgyYWU4NzJ4MDNvc25sOG1lZzE3aiZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/kucy3N9OuBSFvvFxiZ/giphy.gif"
     }
 
-    if emoji in responses:
-        await message.channel.send(responses[emoji])
+    # =====================================
+    # DOUBLE EMOJI COMBO
+    # =====================================
 
-bot.run(token, log_handler=handler, log_level=logging.DEBUG)
+
+
+    if message.id in combo_triggered_messages:
+        return
+
+    reaction_emojis = [str(r.emoji) for r in message.reactions]
+
+    if (
+            "6️⃣" in reaction_emojis
+            and "7️⃣" in reaction_emojis
+            and message.id not in combo_triggered_messages
+    ):
+        combo_triggered_messages.add(message.id)
+
+        await message.channel.send(
+            "https://tenor.com/view/67-6767-676767-cat67-cat-gif-18345003009543756801"
+        )
+
+        return
+    # =====================================
+    # NORMAL EMOJI CHECK
+    # =====================================
+
+    if emoji not in image_map:
+        return
+
+    image = image_map[emoji]
+
+    # =====================================
+    # BOT MESSAGE = EDIT ONLY
+    # =====================================
+
+    if message.author == bot.user:
+
+        # LOCAL FILE
+        if image.startswith("img/"):
+
+            filename = os.path.basename(image)
+
+            file = discord.File(
+                image,
+                filename=filename
+            )
+
+            embed = discord.Embed(
+                description=f"{emoji} by {user.display_name}"
+            )
+
+            embed.set_image(
+                url=f"attachment://{filename}"
+            )
+
+            await message.edit(
+                embed=embed,
+                attachments=[file]
+            )
+
+        # URL IMAGE / GIF
+        else:
+
+            embed = discord.Embed(
+                description=f"{emoji} by {user.display_name}"
+            )
+
+            embed.set_image(url=image)
+
+            await message.edit(
+                embed=embed,
+                attachments=[]
+            )
+
+    # =====================================
+    # NON-BOT MESSAGE = SEND NEW IMAGE
+    # =====================================
+
+    else:
+
+        # LOCAL FILE
+        if image.startswith("img/"):
+
+            await message.channel.send(
+                file=discord.File(image)
+            )
+
+        # URL IMAGE
+        else:
+
+            await message.channel.send(image)
+
+bot.run(
+    token,
+    log_handler=handler,
+    log_level=logging.DEBUG
+)
